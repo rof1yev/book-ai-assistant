737b1de6957a7da2c4d99fa8d830554150fc14f4
rof1yev <rofiyevdilshod@gmail.com>
2026-03-23 13:51:01 +0500

    implement laoding page

---
+++ app/(root)/(home)/loading.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function LoadingPage() {
+  return <div>Loading...</div>;
+}
--- app/(root)/books/new/page.tsx
+++ app/(root)/books/new/page.tsx
@@ -7,7 +7,7 @@ export default function NewBookCreatePage() {
         <section className="flex flex-col gap-5">
           <h1 className="page-title-xl">Add a New Book</h1>
           <p className="subtitle">
-            Upload a PDF to generate your interactive interview
+            Upload a PDF to generate your interactive reading experience
           </p>
         </section>
 
